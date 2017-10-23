4dfd170eccd0bf0551d5ba7db980d347cc48b0d5
Hani <Hani.test@sj.com>
2017-10-23 12:56:47 +0200

    hi MASTER

---
+++ Freitag's Projekt/HelloWorld.c
@@ -0,0 +1,11 @@
+#import <stdio.h>
+#import <stdlib.h>
+#import <conio.h>
+
+int main()
+{
+  printf("Hello World from Master!!");
+
+  return 0;
+}
+
---
+++ Test_GIT/src/Test_GIT.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("I am from MASTER");
+	return 0;
+}
