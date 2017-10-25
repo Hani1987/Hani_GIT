478fbb29cf9852dd65d79aa4cd6037b770b6691d
Hani <Hani.test@sj.com>
2017-10-25 14:04:32 +0200

    New Commit 14:04

---
+++ Call C++ From C/Connector.h
@@ -0,0 +1,15 @@
+#ifndef _CONNECTOR_H
+#define _CONNECTOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void AAA_sayHi(char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
--- Call C++ From C/Old.c
+++ Call C++ From C/Old.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include "FUNCTION_HEADER.h"
 
+
 int main()
 {
-	calculate(5.3, 1.3);
+   calculate(3.1, 3.5);
 
    return 0;
 }
 extern FUNCTION_HEADER void calculate(float num1, float num2)
 {
-  printf("\n\t I am from old C: %.2f\n\n", num1*num2);
+  printf("\n\t I am from old C Function: %.2f\n\n", num1*num2);
 }
