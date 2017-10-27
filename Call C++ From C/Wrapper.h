d642b677752d57743dfe7cf168b6be971ea5e9d6
Hani <Hani.test@sj.com>
2017-10-27 14:31:04 +0200

    123

--- Call C++ From C/Wrapper.h
+++ Call C++ From C/Wrapper.h
@@ -1,12 +1,17 @@
-typedef void CTest;
+#ifndef __MYWRAPPER_H
+#define __MYWRAPPER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef CTest * Student;
-Student ShowData(const  CTest *t);
-Student test_delete(const CTest *t);
+
+typedef struct Student Student;
+
+Student* newStudent();
+
+void ShowData(Student* v);
+
 
 #ifdef __cplusplus
 }
